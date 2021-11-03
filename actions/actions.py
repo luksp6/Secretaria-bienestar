6649df2b575d05e04b05f3e5401a4e30a6625e2a
luksp6 <lucasperez2001@gmail.com>
2021-11-03 13:57:40 -0300

    primer action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,23 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_documentos"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        documento = tracker.latest_message['entities'][0]['value']
+        salida = "Para solicitar tu " + str(documento) + " debes: \n"
+        if str(documento) == "certificado" :
+            salida = salida + "1. Iniciar sesión con tu cuenta en el SIU Guarani"
+        elif str(documento) == 'titulo' :
+            salida = salida + "1. bla bla bla"
+        dispatcher.utter_message(text=str(salida))
+
+        return []
